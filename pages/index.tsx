281ced420f5074298be35a8732a98a37a91fb9d1
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-03-19 09:02:48 +0700

    feat: change btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   };
   return (
     <div className={styles.page}>
-      <button onClick={handleClick} className={styles.button}>Other cats meow</button>
+      <button onClick={handleClick} className={styles.button}>One more cat!</button>
       <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img}/>}</div>
     </div>
   )
